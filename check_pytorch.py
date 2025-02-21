d04bb77bc78da581097c822170f7c64c59981508
selfanti <1752838035@qq.com>
2025-02-21 19:22:54 +0800

    remove submodule

---
+++ check_pytorch.py
@@ -0,0 +1,9 @@
+import torch
+
+# 创建一个 FloatStorage 对象
+float_storage = torch.FloatStorage(5)  # 大小为 5 的浮点数存储对象
+print(float_storage)
+
+# 初始化存储对象中的数据
+float_storage[:] = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5])
+print(float_storage)
