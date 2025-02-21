c3401f2117d929e199c5758b6f7c76bdbb83ecb0
selfanti <1752838035@qq.com>
2025-02-21 19:25:58 +0800

    更改

--- check_pytorch.py
+++ check_pytorch.py
@@ -7,3 +7,10 @@ print(float_storage)
 # 初始化存储对象中的数据
 float_storage[:] = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5])
 print(float_storage)
+print(torch.__version__)
+print(torch.get_default_device())
+a = torch.randn(1, 2, 3, 4, 5)
+torch.numel(a)
+b=a
+b = torch.zeros(4,4)
+print(torch.numel(b))
