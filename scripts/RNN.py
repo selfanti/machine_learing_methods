f54b8b35f0fb500c2a7867eac5170bff9d98a23e
selfanti <1752838035@qq.com>
2025-02-27 17:38:30 +0800

    update

--- scripts/RNN.py
+++ scripts/RNN.py
@@ -69,9 +69,9 @@ def min_max_scaling(x):
 
 if __name__ =="__main__":
     #house和unit分开训练
-    type='house'
+    house_type='house'
     path= r"data/RNN/ma_lga_12345.csv"
-    train_set,test_set=data=load_data(path,type)
+    train_set,test_set=data=load_data(path,house_type)
     print(train_set)
 
 
