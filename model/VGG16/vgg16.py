f2b95e05b11b2cfc2d4f8392af5e06d794ebbf0c
selfanti <1752838035@qq.com>
2025-03-04 11:29:28 +0800

    update

--- model/VGG16/vgg16.py
+++ model/VGG16/vgg16.py
@@ -1,3 +1,73 @@
 import torch
 import torch.nn as nn
 import numpy as np
+
+class VGG16(nn.Module):
+    def __init__(self, num_classes=1000):
+        super().__init__()
+        #特征层
+        self.features=nn.Sequential(
+            #Block1 (2卷积层)
+            nn.Conv2d(3,64,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64,64,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            #Block2 (2卷积层)
+            nn.Conv2d(64,128,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128,128,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            #Block3 (3卷积层)
+            nn.Conv2d(128,256,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            #Block4 (3卷积层)
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Block5 (3卷积层)
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+        )
+        #分类层
+        self.classifier=nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_classes)
+        )
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m,nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight,mode='fan_out',nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias,0)
+            elif isinstance(m,nn.Linear):
+                nn.init.normal_(m.weight,0,0.01)
+                nn.init.constant_(m.bias,0)
+
+    def forward(self,x):
+        x=self.features(x)
+        x=torch.flatten(x,1)
+        x=self.classifier(x)
